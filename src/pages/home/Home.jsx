518d33398281028b7706c7577c27ef2a5bb2894b
Gytis97 <110969822+Gytis97@users.noreply.github.com>
2022-09-05 10:51:35 +0300

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,9 @@
 import MainHeader from "../../components/MainHeader";
 import Programs from "../../components/Programs";
-import Card from "../../UI/Card";
 import Values from "../../components/Values";
 import "./home.css";
 import FAQs from "../../components/FAQs";
 import FeedBack from "../../components/FeedBack";
-import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
