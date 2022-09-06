36318bf677109908529cf4546a16fbe332b338ca
Gytis97 <110969822+Gytis97@users.noreply.github.com>
2022-09-06 10:56:36 +0300

    adding validation

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,40 +3,90 @@ import HeaderImage from "../../images/header_bg_2.jpg";
 import { MdEmail } from "react-icons/md";
 import { BsMessenger } from "react-icons/bs";
 import { IoLogoWhatsapp } from "react-icons/io";
+import { useState } from "react";
 
 import "./contact.css";
 
 const Contact = () => {
+  const [index, setIndex] = useState(false);
+  const [name, setName] = useState(false);
+  const [textarea, setTextarea] = useState(false);
+
+  const prevFeedbackHandler = () => {
+    var email = document.getElementById("emailInput").value;
+    var name = document.getElementById("textInput").value;
+    var textarea = document.getElementById("areaInput").value;
+    if (email === "") {
+      setIndex(true);
+    }
+    if (email !== "") {
+      setIndex(false);
+    }
+
+    if (name === "") {
+      setName(true);
+    }
+    if (name !== "") {
+      setName(false);
+    }
+
+    if (textarea === "") {
+      setTextarea(true);
+    }
+    if (textarea !== "") {
+      setTextarea(false);
+    }
+  };
+
   return (
     <>
-      <Header title="Susisiekite" image={HeaderImage}>
+      <Header title="" image={HeaderImage}>
         <p className="contact__headerp">
           Jeigu kilo klausimu susijusių su sporto klubu, ar turite įdėjų kurias
-          norėtumėte įgyvendinti kartu su mumis - rašykite apačioje.
+          norėtumėte įgyvendinti kartu - rašykite apačioje.
         </p>
 
         <form>
           <div className="input__name">
             <input
+              id="textInput"
               type="text"
               name="First Name"
               placeholder="Jūsų Vardas"
               required
             />
           </div>
+
+          <span className={name ? "show" : "validation"}>
+            užpildykite laukelį
+          </span>
           <div className="input__email">
             <input
+              id="emailInput"
               type="email"
               name="Email Address"
               placeholder="Jūsų el. paštas"
               required
             />
           </div>
+          <span className={index ? "show" : "validation"}>
+            užpildykite laukelį
+          </span>
           <div className="message__input">
-            <textarea rows="4" placeholder="Jūsų tekstas"></textarea>
+            <textarea
+              id="areaInput"
+              rows="4"
+              placeholder="Jūsų tekstas"
+              required
+            ></textarea>
           </div>
+          <span className={textarea ? "show" : "validation"}>
+            užpildykite laukelį
+          </span>
           <div className="input__btn">
-            <button className="btn">Siųsti</button>
+            <button className="btn" onClick={prevFeedbackHandler}>
+              Siųsti
+            </button>
           </div>
         </form>
       </Header>
