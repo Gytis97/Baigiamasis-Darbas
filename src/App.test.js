0941e747f7574f638ebcb3e527e351c124a59cbc
Gytis97 <110969822+Gytis97@users.noreply.github.com>
2022-09-08 15:56:15 +0300

    Delete App.test.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("Test if Treniruociu planai exists", () => {
-  render(<App />);
-  const linkElement = screen.getByText("Treniruočių planai");
-  expect(linkElement).toBeInTheDocument();
-});
